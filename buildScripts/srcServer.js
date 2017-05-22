558d45a17211e9a3d1a744e4159708efa8757f44
Denver Coder <timmyers1978@gmail.com>
2017-05-21 23:16:26 -0600

    add http api processing

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1, "firstName":"Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName":"Steve", "lastName": "Perry", "email": "steve@gmail.com"},
+    {"id": 3, "firstName":"Billy", "lastName": "King", "email": "billy@gmail.com"}
+  ]);
+});
+
 /* eslint-disable no-console */
 
 app.listen(port, function(err) {
