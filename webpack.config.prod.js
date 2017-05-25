af3ae90615bfb40c7a2ca285beb6cae5e8b631af
Denver Coder <timmyers1978@gmail.com>
2017-05-25 15:06:04 -0600

    busting cache on js files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,21 +1,33 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMD5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
     debug: true,
     devtool: 'source-map',
     noInfo: false,
-    entry: [
-        path.resolve(__dirname, 'src/index')
-    ],
+    entry: {
+        vendor: path.resolve(__dirname, 'src/vendor'),
+        main: path.resolve(__dirname, 'src/index')
+    },
     target: 'web',
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: 'bundle.js'
+        filename: '[name].[chunkhash].js'
     },
     plugins: [
+        
+        new ExtractTextPlugin('[name].[contenthash].css'),
+
+        new WebpackMD5Hash(),
+        
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+        
         new HtmlWebpackPlugin({
             template: 'src/index.html',
             minify: {
@@ -32,13 +44,15 @@ export default {
             },
             inject: true
         }),
+
         new webpack.optimize.DedupePlugin(),
+        
         new webpack.optimize.UglifyJsPlugin()
     ],
     module: {
         loaders: [
             {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-            {test: /\.css$/, loaders: ['style', 'css']}
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
         ]
     }
 }
